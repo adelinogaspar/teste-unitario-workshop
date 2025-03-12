e3d92d4e7f55c4fe7594ef131cfcb10aac7312c5
Gaspar <adelinogaspar@gmail.com>
2025-03-12 20:43:29 -0300

    removendo imports não usados

--- src/main/java/br/com/itau/workshop/facade/AlunoFacade.java
+++ src/main/java/br/com/itau/workshop/facade/AlunoFacade.java
@@ -5,16 +5,12 @@ import br.com.itau.workshop.facade.exception.AlunoJaCadastradoException;
 import br.com.itau.workshop.mapper.AlunoMapper;
 import br.com.itau.workshop.repository.AlunoRepository;
 import br.com.itau.workshop.repository.entity.AlunoEntity;
-import br.com.itau.workshop.repository.entity.HabilidadeEntity;
 import br.com.itau.workshop.util.Util;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.util.Assert;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Set;
-import java.util.UUID;
 
 @Service
 public class AlunoFacade {
