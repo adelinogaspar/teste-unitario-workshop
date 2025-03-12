5024c022422cb76a9dfa2c28bcaf9f006852c759
Gaspar <adelinogaspar@gmail.com>
2025-03-12 20:55:52 -0300

    inclusão do jacoco

--- src/test/java/br/com/itau/workshop/ApplicationTests.java
+++ src/test/java/br/com/itau/workshop/ApplicationTests.java
@@ -1,13 +1,22 @@
 package br.com.itau.workshop;
 
+import br.com.itau.workshop.facade.AlunoFacade;
+import br.com.itau.workshop.repository.entity.AlunoEntity;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class ApplicationTests {
 
+	@Autowired
+	AlunoFacade alunoFacade;
+
 	@Test
 	void contextLoads() {
+		assertNotNull(alunoFacade);
 	}
 
 }
